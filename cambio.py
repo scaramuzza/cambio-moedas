204b22df365a3dbd07dab03f91d85169ba8d960c
Marcos Scaramussa Jr <marcos.scaramussa.jr@gmail.com>
2019-03-31 15:28:21 -0300

    Quarta Alteração: Código para Exportar arquivo CSV

--- cambio.py
+++ cambio.py
@@ -3,6 +3,8 @@
 
 import requests
 import json
+import pandas as pd # 'as pd' é um apelido que você está colocando no panda >>> pd
+import decimal
 
 url = "http://data.fixer.io/api/latest?access_key=1454bbcb8173055a6dee5046c5307d3a"
 print("Acessando base de dados...")
@@ -20,7 +22,7 @@ if response.status_code == 200:
     print("")
     print("Base de dados acessada com sucesso")
     print("Buscando informações das moedas...")
-    dados = response.json() # Desempacotar o JSON
+    dados = response.json() # Desempacotar o JSON, que é um dicionário
     
     day = dados ["date"] # Poderia ser com aspas simples também
     print ("Acesssando dados do dia %s/%s/%s" % (day [8:], day [5:7], day [0:4]))
@@ -36,14 +38,25 @@ if response.status_code == 200:
     
     # Para executar somente o 'rates' ou valores da moedas. Rates é um dicionário dentro de outro
 
-    euro_real = dados['rates']['BRL'] / dados['rates']['EUR']
+    euro_real = round ( dados['rates']['BRL'] / dados['rates']['EUR'], 2)
     print(euro_real)
 
-    dollar_real = dados['rates']['BRL'] / dados['rates']['USD']
+    dollar_real = round ( dados['rates']['BRL'] / dados['rates']['USD'], 2)
     print(dollar_real)
 
-    btc_real = dados['rates']['BRL'] / dados['rates']['BTC']
+    btc_real = round ( dados['rates']['BRL'] / dados['rates']['BTC'], 2)
     print(btc_real)
+
+    df = pd.DataFrame ({'Moedas':['Euro', 'Dollar', 'Bitcoin'], 'Valores':[euro_real,dollar_real,btc_real]})
+    df.to_csv("valores.csv", index=False, sep=";") 
+    
+    # DataFrame é para geração de uma tabela 
+    # index=False é para não mostrar a primeira coluna chama index
+    # sep=";" ele separa em duas colunas o que está dividido em ponto e vírgula
+
+    print("")
+    print("Arquivo Exportado com Sucesso !!!")
+
 else:
     print("Site com Problemas! Não deu 200")
 
